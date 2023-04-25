09cae69df9f212d37af6f1364feb3f0910a53120
kdmitrie <presentatio@mail.ru>
2023-04-25 10:21:18 +0300

    transfer_x_to_device parameter in ModelTrainer

--- trainer.py
+++ trainer.py
@@ -63,6 +63,9 @@ class ModelTrainer:
     # What to calculate and what rate to use
     metrics: List[ModelMetrics] = field(default_factory=list)
         
+    # Whether to transfer data on GPU before applying the model
+    transfer_x_to_device : Boolean = True
+        
     validation_calculate_rate: int = 1 # 0 for never calculating validation metrics
     print_rate: int = 1 # 0 for never printing metrics
     limit_batches_per_epoch: int = 0 # if not zero, stop epoch on this value
@@ -73,7 +76,7 @@ class ModelTrainer:
         """Applies the model and returns an array of predictions and a tensor of loss values"""
 
         # 1. We calculate the model result on each minibatch
-        x_gpu = x.to(self.device)
+        x_gpu = x.to(self.device) if self.transfer_x_to_device else x
         prediction = model(x_gpu)
         
         # 2. We calculate the loss function
